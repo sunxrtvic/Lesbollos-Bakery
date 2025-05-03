c7001b6cdcdfc8eeda8dddfa543b955486c2d6f2
Victoria <vrg_rodriguez@hotmail.com>
2025-05-03 18:07:02 +0200

    feat: creada página y estilo del carrito de la compra

---
+++ assets/carrito.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <title>LesBollos Bakery</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css">
+    <link rel="stylesheet" href="./css/styleuser.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css">
+</head>
+
+<body>
+    <?php
+    include_once('encabezado.php');
+    ?>
+
+    <main>
+
+    </main>
+
+    <?php
+    include_once "footer.php";
+    ?>
+
+</body>
