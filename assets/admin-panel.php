238827a98961ed2610f5915f18a26a08c2d2b643
Victoria <vrg_rodriguez@hotmail.com>
2025-05-25 02:12:48 +0200

    Modificado más fallos de estilo en admin-panel

--- assets/admin-panel.php
+++ assets/admin-panel.php
@@ -19,14 +19,6 @@ $usuario = $_SESSION['usuario'];
     <link rel="stylesheet" href="./css/styleperfil.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css">
     <style>
-        html,
-        body {
-            height: 100%;
-            margin: 0;
-            padding: 0;
-            display: flex;
-            flex-direction: column;
-        }
 
         h1 {
             padding-bottom: 1.875rem;
